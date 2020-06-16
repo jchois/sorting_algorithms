5e181a78a9ab4be7f638794a83fe550207bc7ded
jchois <choisjuliana@gmail.com>
2020-06-16 02:03:02 -0500

    some code of task 2 added

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in ascending order
+ * using the Quick sort algorithm
+ * @array: array to sort
+ * @size: array sixe
+ * Return: nothing
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	size_t i, j, num_min;
+	int aux;
+
+	if (array == NULL || !size)
+	{
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		num_min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[num_min])
+				num_min = j;
+		}
+		aux = array[i];
+		array[i] = array[num_min];
+		array[num_min] = aux;
+		print_array(array, size);
+	}
+}

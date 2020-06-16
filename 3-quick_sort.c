83c4ceac1924b928b18c054eca09ff7200509230
jchois <choisjuliana@gmail.com>
2020-06-16 18:02:39 -0500

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,58 @@
 #include "sort.h"
 
 /**
+ * partition - divide using the pivot
+ * @array: array to be sorted
+ * @left: the lower position
+ * @right: the higher position
+ * @size: array size
+ * Return: partitioned element
+ */
+int partition(int *array, int left, int right, size_t size)
+{
+	int pivot, part, i = left, aux;
+
+	pivot = array[right];
+	part = left - 1;
+
+	while (i <= right)
+	{
+		if (array[i] <= pivot)
+		{
+			part++;
+			if (part != i)
+			{
+				aux = array[part];
+				array[part] = array[i];
+				array[i] = aux;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+	return (part);
+}
+
+/**
+ * sorting - validate each position of array
+ * @array: array to be sorted
+ * @left: the lower position
+ * @right: the higher position
+ * @size: array size
+ * Return: nothing
+ */
+void sorting(int *array, int left, int right, size_t size)
+{
+	if (left < right)
+	{
+		int pi = partition(array, left, right, size);
+
+		sorting(array, left, pi - 1, size);
+		sorting(array, pi + 1, right, size);
+	}
+}
+
+/**
  * quick_sort -  sorts an array of integers in ascending
  * order using the Quick sort algorithm
  * @array: array to sort
@@ -9,67 +61,14 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	int low, high;
+	int right = size - 1, left = 0;
 
-	low = 0;
-	high = size - 1;
-
-	if (array == NULL || !size)
-		return;
-
-	sorting(array, low, high, size);
-}
-
-/**
- * sorting - validate each position of array
- * @array: array to be sorted
- * @low: the lower position
- * @high: the higher position
- * @size: array size
- * Return: nothing
- */
-void sorting(int *array, int low, int high, size_t size)
-{
-	if (low > high)
-		return;
-
-	int p = partition(array, low, high, size);
-
-	sorting(array, low, p - 1, size);
-	sorting(array, p + 1, high, size);
-
-}
-
-/**
- * partition - divide using the pivot
- * @array: array to be sorted
- * @low: the lower position
- * @high: the higher position
- * @size: array size
- * Return: partitioned element
- */
-int partition(int *array, int low, int high, size_t size)
-{
-	int i, pos, swap, pivot;
-
-	i = low;
-	pos = low - 1;
-	pivot = array[high];
-
-	while (i <= high)
+	if (!array || !size)
 	{
-		if (array[i] <= pivot)
-		{
-			pos++;
-			if (pos != i)
-			{
-				swap = array[pos];
-				array[pos] = array[i];
-				array[i] = swap;
-				print_array(array, size);
-			}
-		}
-		i++;
+		return;
 	}
-	return (pos);
+	else
+	{
+		sorting(array, left, right, size);
+	}
 }
